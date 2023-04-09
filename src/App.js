d2e1e5d48f4e8ade23df30e7882831fe1a53ccb1
Jinhyeon Kwon <121283722+jinhyeonkwon@users.noreply.github.com>
2023-04-09 11:23:37 +0900

    Finish

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ToDo from './Todo.js';
 const App = () => {
   const [name, setName] = useState(''); // 각각의 할 일의 이름
   const [importance, setImportance] = useState('h'); // 각각의 할 일의 중요도
+  const [content, setContent] = useState('') // 각각의 할 일의 내용
   const [due, setDue] = useState(''); // 각각의 할 일의 기한
   const [id, setId] = useState(0);
   const [toDos, setToDos] = useState([]); // 각각의 할 일에 대한 정보들이 담긴 객체들의 리스트
@@ -16,79 +17,110 @@ const App = () => {
     console.log(event);
   };
 
+  const removeToDo = (event) => {
+    console.log(`remove fn called with ${event.target.id}`);
+    const idToRemove = parseInt(event.target.id);
+    setToDos((currToDos) => currToDos.filter((toDo) => toDo.id !== idToRemove));
+  }; // 뭘 지울지는 toDo가 가진 버튼에서.. -> 이 함수를 toDo의 props로 넘겨준다.
+
   const onSubmit = (event) => {
     event.preventDefault();
     const newToDo = {};
     newToDo.id = id;
     setId((id) => id + 1);
     newToDo.name = name;
+    newToDo.content = content;
     newToDo.importance = importance;
     newToDo.due = due;
     setToDos((currToDos) => [newToDo, ...currToDos]);
   };
   console.log(toDos);
   return (
-    <div className="Header">
-      <h1>To-do List</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          className="Input Name"
-          type="text"
-          required
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-          placeholder="할 일을 입력하세요.."
-          name="name"
-        ></input>
-        <br />
+    <div>
+      <div className="Header">
+        <h1>To-do List</h1>
+        <form onSubmit={onSubmit}>
+          <div>
+            <input
+              className="Input Name"
+              type="text"
+              required
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+              placeholder="할 일을 입력하세요.."
+              name="name"
+            ></input>
+          </div>
+          <br></br>
+          <div>
+            중요도
+            <select
+              name="importance"
+              type="text"
+              required
+              onChange={(event) => {
+                setImportance(event.target.value);
+              }}
+            >
+              <option value="h">높음</option>
+              <option value="m">중간</option>
+              <option value="l">낮음</option>
+            </select>
+          </div>
+          <br></br>
+          <div>
+          <input
+              className="Input Content"
+              type="text"
+              onChange={(event) => {
+                setContent(event.target.value);
+              }}
+              placeholder="내용"
+              name="content"
+            ></input>
+          </div>
+          <br></br>
+          <div>
+            기한
+            <input
+              className="Input Due"
+              type="date"
+              required
+              onChange={(event) => {
+                setDue(event.target.value);
+              }}
+              name="due"
+            ></input>
+          </div>
+          <br></br>
+          <button type="submit">Todo 추가하기</button>
+        </form>
+      </div>
+      <div class="filter">
         <p>
-          중요도
+          중요도 필터 설정{' '}
           <select
-            name="importance"
+            name="importance-filter"
             type="text"
             required
             onChange={(event) => {
-              setImportance(event.target.value);
+              setImportanceFilter(event.target.value);
             }}
           >
+            <option value="a">전체</option>
             <option value="h">높음</option>
             <option value="m">중간</option>
             <option value="l">낮음</option>
           </select>
         </p>
-        <p>
-          기한
-          <input
-            className="Input Due"
-            type="date"
-            required
-            onChange={(event) => {
-              setDue(event.target.value);
-            }}
-            name="due"
-          ></input>
-        </p>
-        <button type="submit">Todo 추가하기</button>
-      </form>
-      <p>
-        중요도 필터 설정{' '}
-        <select
-          name="importance-filter"
-          type="text"
-          required
-          onChange={(event) => {
-            setImportanceFilter(event.target.value);
-          }}
-        >
-          <option value="a">전체</option>
-          <option value="h">높음</option>
-          <option value="m">중간</option>
-          <option value="l">낮음</option>
-        </select>
-      </p>
+      </div>
 
-      <ToDo todos={toDos} importancefilter={importanceFilter} />
+      <ToDo
+        todos={toDos}
+        removeToDo={removeToDo}
+        importancefilter={importanceFilter}
+      />
     </div>
   );
 };
--- src/Todo.js
+++ src/Todo.js
@@ -13,23 +13,32 @@ const ToDo = (props) => {
     return false;
   });
   console.log('in todo', filteredToDoList);
+  const importanceStr = new Map();
+  importanceStr.set('h', '높음');
+  importanceStr.set('m', '중간');
+  importanceStr.set('l', '낮음');
   const toDoList = filteredToDoList.map((toDo) => (
     <tr key={toDo.id}>
       <td>{toDo.name}</td>
-      <td>{toDo.importance}</td>
+      <td>{importanceStr.get(toDo.importance)}</td>
+      <td>{toDo.content}</td>
       <td>{toDo.due}</td>
       <td>
-        <button>Delete</button>
+        <button id={toDo.id} onClick={(event) => props.removeToDo(event)}>
+          Delete
+        </button>
       </td>
     </tr>
   ));
   return (
-    <table>
+    <table className="todo-table">
       <thead>
         <tr>
-          <th>ToDo 항목</th>
-          <th>중요도</th>
-          <th>기한</th>
+          <th width="150px">ToDo 항목</th>
+          <th width="50px">중요도</th>
+          <th width="300px">내용</th>
+          <th width="90px">기한</th>
+          <th width="70px">삭제하기</th>
         </tr>
       </thead>
       <tbody>{toDoList}</tbody>
